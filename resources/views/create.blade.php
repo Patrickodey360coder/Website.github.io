ed7959ed4d33c8a2619d78beb8e4ee5ffa4d91ca
patrickodey <patrickodey419@gmail.com>
2021-10-10 03:09:39 +0100

    fix: create

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -3,8 +3,8 @@
 @section('content')
   <form action="/products" method="POST" enctype="multipart/form-data">
     @csrf
-    <input class="form-control" type="text" placeholder="product name" name="name">
-    <input class="form-control" type="text" placeholder="description" name="description">
+    <input class="form-control" type="text" placeholder="product name" name="name" >
+    <input class="form-control" type="text" placeholder="description" name="description" >
     <input class="form-control" type="text" placeholder="price" name="price" >
     <input class="form-control" type="file" placeholder="price" name="image" >
     <input class="form-control" type="submit" value="Add" >
