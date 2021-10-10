431721b23d14e0cb85418b2f462d9c903396e665
patrickodey <patrickodey419@gmail.com>
2021-10-10 15:01:38 +0100

    fix: form

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -7,6 +7,6 @@
     <input class="form-control" type="text" placeholder="description" name="description">
     <input class="form-control" type="text" placeholder="price" name="price">
     <input class="form-control" type="file" placeholder="price" name="image">
-    <input class="form-control" type="submit" value="Add">
+    <input class="form-control" type="submit" value="Add" >
   </form>
 @endsection
