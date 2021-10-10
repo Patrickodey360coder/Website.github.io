410fa08fda7d5fb033a938fd976bb5efa53fefac
patrickodey <patrickodey419@gmail.com>
2021-10-10 03:10:08 +0100

    fix: create

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -5,7 +5,7 @@
     @csrf
     <input class="form-control" type="text" placeholder="product name" name="name">
     <input class="form-control" type="text" placeholder="description" name="description">
-    <input class="form-control" type="text" placeholder="price" name="price" >
+    <input class="form-control" type="text" placeholder="price" name="price">
     <input class="form-control" type="file" placeholder="price" name="image" >
     <input class="form-control" type="submit" value="Add" >
   </form>
